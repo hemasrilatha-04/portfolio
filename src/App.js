e371fab58c95f1f76c2ce01f6ecc26d1135ebe23
Hema Srilatha <you@example.com>
2026-05-01 10:48:55 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hi, I'm Hema 👋</h1>
+      <p>Computer Science Student | Web Developer</p>
+
+      <div className="links">
+        <a href="https://www.codechef.com/users/hema_50" target="_blank" rel="noopener noreferrer">CodeChef</a>
+        <a href="https://www.linkedin.com/in/hemasrilatha-makkena-0065aa32a/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="https://github.com/hemasrilatha-04" target="_blank" rel="noopener noreferrer">GitHub</a>
+      </div>
     </div>
   );
 }
